b78c2180192f414afed457c4a778b4f9fb6e4123
Kieran Bamforth <hello@kieranbamforth.me>
2015-10-04 18:59:14 +0100

    psr4 autoloading src.

---
+++ src/Tests/Model/CustomerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use KieranBamforth\CustomerInviter\Model\Customer;
+
+class CustomerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSet()
+    {
+        $userId = 1;
+        $name = 'Kieran Bamforth';
+        $lng = -6.043701;
+        $lat = 52.986375;
+
+        $customer = new Customer();
+        $customer->setUserId($userId)
+            ->setName($name)
+            ->setLongitude($lng)
+            ->setLatitude($lat);
+
+        $this->assertEquals(
+            $userId,
+            $customer->getUserId();
+        );
+
+        $this->assertEquals(
+            $name,
+            $customer->getName();
+        );
+
+        $this->assertEquals(
+            $lng,
+            $customer->getLongitude();
+        );
+
+        $this->assertEquals(
+            $lat,
+            $customer->getLatitude();
+        );
+    }
+}
