0d2ade2d5c165d5466b61d8cb364a5ac56b2cb85
Kieran Bamforth <hello@kieranbamforth.me>
2015-10-05 20:51:23 +0100

    Adding tests for distance calculator.

--- src/Tests/DistanceCalculator/DistanceCalculatorTest.php
+++ src/Tests/DistanceCalculator/DistanceCalculatorTest.php
@@ -12,6 +12,46 @@ class DistanceCalculatorTest extends \PHPUnit_Framework_TestCase
     {
         $this->distanceCalculator = new DistanceCalculator();
     }
+
+
+    /**
+     * Tests that a given location is close to Intercom Dublin office.
+     *
+     * @dataProvider harversineGreatCircleDistanceDataProvider
+     *
+     * @param $longTo
+     * @param $latTo
+     * @param $expected
+     *
+     * @return void
+     */
+    public function testHaversineGreatCircleDistance($latTo, $longTo, $expected)
+    {
+        $intercomOfficeLat = 53.3381985;
+        $intercomOfficeLng =  -6.2592576;
+
+        $result = $this->distanceCalculator->haversineGreatCircleDistance(
+            $intercomOfficeLat,
+            $intercomOfficeLng,
+            $latTo,
+            $longTo,
+            6371
+        );
+
+        $this->assertEquals($expected, round($result,2));
+    }
+
+    /**
+     * These expectations come from http://www.movable-type.co.uk/scripts/latlong.html.
+     */
+    public function harversineGreatCircleDistanceDataProvider()
+    {
+        return [
+            [52.986375, -6.043701, 41.68],
+            [51.92893, -10.27699, 313.10]
+        ];
+    }
+
     /**
      * @dataProvider degreesToRadiansDataProvider
      *
